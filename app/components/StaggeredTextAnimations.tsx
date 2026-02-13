1853601b38c92759d0615be18f371ec57d7e51b2
P-Appadoo <171328746+P-Appadoo@users.noreply.github.com>
2026-02-13 14:54:43 +0400

    feat: added staggered text animations on focus

--- app/components/StaggeredTextAnimations.tsx
+++ app/components/StaggeredTextAnimations.tsx
@@ -29,6 +29,7 @@ const FlipLink = ({ children, href }: { children: string; href: string }) => {
     <motion.a
       initial="initial"
       whileHover="hovered"
+      whileFocus="hovered"
       href={href}
       className="relative block overflow-hidden whitespace-nowrap text-4xl font-black uppercase sm:text-7xl md:text-8xl lg:text-9xl"
       // style={{lineHeight: 0.75}}
